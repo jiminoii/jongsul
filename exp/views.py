58942dec9063478edfe5ab898d9ecfdeb72b5a72
chiouktae <cyt483566@gmail.com>
2020-08-12 15:40:11 +0900

    ok

--- exp/views.py
+++ exp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import JsonResponse # JSON 응답
+from exp.models import Point
+from django.forms.models import model_to_dict
 
-# Create your views here.
+
+def expmap_data(request):
+
+    data = Point.objects.all()
+    map_list = []
+    for d in data:
+        d = model_to_dict(d) # QuerySet -> Dict
+        map_list.append(d)
+    return JsonResponse(map_list, safe=False)
+
