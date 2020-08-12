2ab5b064dfedb780c5f5bfa5feb7bd64bb7ea607
dlalth <dlalth@naver.com>
2020-08-12 17:31:14 +0900

    asdf

--- exp/models.py
+++ exp/models.py
@@ -10,4 +10,3 @@ class Inpo(models.Model): #제가 몰라서 오타낸게 아닙니다 ㅠㅠㅠ
     name = models.CharField(max_length=100)
     adress = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
-
