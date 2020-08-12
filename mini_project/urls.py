02c1f24fed6c13006f4a7baf46d70d280ca1b2b8
mechanicdong <mechanicdong@gmail.com>
2020-08-12 15:15:30 +0900

    food002

--- mini_project/urls.py
+++ mini_project/urls.py
@@ -5,12 +5,8 @@ import mini_project.views as views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-    path('food/', views.food),
+    path('food/<int:id>', views.food),
     path('stay/', views.stay),
     path('festival/', views.festival),
-    path('exp/', views.exp),
-<<<<<<< HEAD
+    path('exp/', views.exp)
 ]
-=======
-]
->>>>>>> fb4cfae32d8c88a2ef593e18bd849bdf6f41eb53
--- mini_project/views.py
+++ mini_project/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponse,HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
-
+from food.models import Food_Point
 def index(request):
     return render(request, 'index.html')
 
 def food(request):
-    return render(request, 'food.html')
+    food_point = Food_Point.objects.order_by('-id')
+    context = {
+        'food_point' : food_point
+    }
+    return render(request, 'food.html', context)
+
 
 def exp(request):
     return render(request, 'exp.html')
