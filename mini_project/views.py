25306c344bde4c3e7a656e739a8eb73f44478363
chiouktae <cyt483566@gmail.com>
2020-08-12 17:35:01 +0900

    stay list update

--- mini_project/views.py
+++ mini_project/views.py
@@ -39,7 +39,77 @@ def stay(request):
             break
         e_table = result.find('</table>',s_table)
         star += '<div class="jumbotron"><table class="table table-hover"'+result[s_table:e_table+8]+'</div>'
+    result = requests.get('https://www.tourandong.com/public/sub3/sub2_2.cshtml')
+    result.encoding = 'utf-8'
+    result = result.text
+    s_table = 0
+    e_table = 0
+    star1 = ""
+    while True:
+        s_table = result.find('<table',e_table)
+        s_table = result.find('>',s_table)
+        if s_table == -1:
+            break
+        e_table = result.find('</table>',s_table)
+        star1 += '<div class="jumbotron"><table class="table table-hover"'+result[s_table:e_table+8]+'</div>'
+    result = requests.get('https://www.tourandong.com/public/sub3/sub2_3.cshtml')
+    result.encoding = 'utf-8'
+    result = result.text
+    s_table = 0
+    e_table = 0
+    star2 = ""
+    while True:
+        s_table = result.find('<table',e_table)
+        s_table = result.find('>',s_table)
+        if s_table == -1:
+            break
+        e_table = result.find('</table>',s_table)
+        star2 += '<div class="jumbotron"><table class="table table-hover"'+result[s_table:e_table+8]+'</div>'
+    result = requests.get('https://www.tourandong.com/public/sub3/sub2_4.cshtml')
+    result.encoding = 'utf-8'
+    result = result.text
+    s_table = 0
+    e_table = 0
+    star3 = ""
+    while True:
+        s_table = result.find('<table',e_table)
+        s_table = result.find('>',s_table)
+        if s_table == -1:
+            break
+        e_table = result.find('</table>',s_table)
+        star3 += '<div class="jumbotron"><table class="table table-hover"'+result[s_table:e_table+8]+'</div>'
+    result = requests.get('https://www.tourandong.com/public/sub3/sub2_1.cshtml')
+    result.encoding = 'utf-8'
+    result = result.text
+    s_table = 0
+    e_table = 0
+    star4 = ""
+    while True:
+        s_table = result.find('<table',e_table)
+        s_table = result.find('>',s_table)
+        if s_table == -1:
+            break
+        e_table = result.find('</table>',s_table)
+        star4 += '<div class="jumbotron"><table class="table table-hover"'+result[s_table:e_table+8]+'</div>'
+    result = requests.get('https://www.tourandong.com/public/sub3/sub2_2_1.cshtml')
+    result.encoding = 'utf-8'
+    result = result.text
+    s_table = 0
+    e_table = 0
+    star5 = ""
+    while True:
+        s_table = result.find('<table',e_table)
+        s_table = result.find('>',s_table)
+        if s_table == -1:
+            break
+        e_table = result.find('</table>',s_table)
+        star5 += '<div class="jumbotron"><table class="table table-hover"'+result[s_table:e_table+8]+'</div>'
     r_ta = {
-        'contact' : star
+        'contact' : star,
+        'contact1' : star1,
+        'contact2' : star2,
+        'contact3' : star3,
+        'contact4' : star4,
+        'contact5' : star5,
     }
     return render(request, 'stay.html',r_ta)
